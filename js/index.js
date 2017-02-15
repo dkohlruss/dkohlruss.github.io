4f0590fc0120bfab2f7776c2f705acca896a39c0
David Kohlruss <dkohlruss@gmail.com>
2017-02-15 15:39:08 -0700

    Initial Mobile Commit

--- js/index.js
+++ js/index.js
@@ -2,13 +2,18 @@ $( document ).ready(function() {
     var $intro = $('.intro');
     $('.firstbelow').css('margin-top', $intro.height()).css('width', $intro.width() );
 
+    if ($(window).width() < 768) {
+        $('.intro').css('background','rgb(50,50,50)');
+    }
 });
 
 $( window ).resize(function() {
     var $intro = $('.intro');
     $('.firstbelow').css('margin-top', $intro.height()).css('width', $intro.width() );
-});
 
-$('button').click(function() {
-    console.log('clickly!');
+    if ($(window).width() < 768) {
+        $('.intro').css('background','rgb(50,50,50)');
+    } else {
+        $('.intro').css('background', 'none');
+    }
 });
