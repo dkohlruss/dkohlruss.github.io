4ba6a2b9cf7e70b12efea3ded1e7ba46c3f1b1c0
dkohlruss <dkohlruss@gmail.com>
2017-12-16 09:25:16 -0700

    Get request to ecommerce backend added

--- js/index.js
+++ js/index.js
@@ -1,23 +1,32 @@
-$( document ).ready(function() {
-    var $intro = $('.intro');
-    $('.firstbelow').css('margin-top', $intro.height()).css('width', $intro.width() );
+$(document).ready(function() {
+	// Make a call to API to spin up the backend for eCommerce web apple
+	$.get('https://ecommerceback.herokuapp.com/', function(data) {
+		console.log('Get request to eCommerce backend to spin up heroku');
+	});
 
-    if ($(window).width() < 768) {
-        $('.intro').css('background','rgb(50,50,50)');
-    }
+	var $intro = $('.intro');
+	$('.firstbelow')
+		.css('margin-top', $intro.height())
+		.css('width', $intro.width());
+
+	if ($(window).width() < 768) {
+		$('.intro').css('background', 'rgb(50,50,50)');
+	}
 });
 
-$( window ).resize(function() {
-    var $intro = $('.intro');
-    $('.firstbelow').css('margin-top', $intro.height()).css('width', $intro.width() );
+$(window).resize(function() {
+	var $intro = $('.intro');
+	$('.firstbelow')
+		.css('margin-top', $intro.height())
+		.css('width', $intro.width());
 
-    if ($(window).width() < 768) {
-        $('.intro').css('background','rgb(50,50,50)');
-    } else {
-        $('.intro').css('background', 'none');
-    }
+	if ($(window).width() < 768) {
+		$('.intro').css('background', 'rgb(50,50,50)');
+	} else {
+		$('.intro').css('background', 'none');
+	}
 
-    if ($(window).height < 550) {
-        $('.logo').css('display','none');
-    }
+	if ($(window).height < 550) {
+		$('.logo').css('display', 'none');
+	}
 });
